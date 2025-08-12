864204950bb0d61ce5250d41c4f82537557e83e7
Insaf Lashari <126097535+insaflashari@users.noreply.github.com>
2025-08-12 13:56:09 -0400

    fixing backend

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = "/choreo-apis/choose-your-own-adventure/backend/v1/api"
 
 //for it to work locally do export const API_BASE_URL = "/api"
+///choreo-apis/choose-your-own-adventure/backend/v1/
