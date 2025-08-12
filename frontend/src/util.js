1c13781b97eace650c7e09d1d35da697d69c25e4
Insaf Lashari <126097535+insaflashari@users.noreply.github.com>
2025-08-11 23:40:29 -0400

    changed url

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "/api"
+export const API_BASE_URL = "/choreo-apis/choose-your-own-adventure/backend/v1/api"
+
+//for it to work locally do export const API_BASE_URL = "/api"
